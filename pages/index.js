73807ea05e6e371ef39d1a120fece431a27e8bf7
hitochix06 <pachara.philibert@hotmail.fr>
2023-04-12 15:26:19 +0200

    petit changment

--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,11 @@ import Footer from '@/components/Footer';
 export default function Home() {
   return (
     <>
-          <Head>
+      <Head>
         <title>Philibert Pachara- Développeur Full-Stack Javascript en Alternance</title>
         <meta name="description" content="Développeur web JavaScript en alternance, compétent en front-end et back-end, avec expérience en React et Node.js. Contactez-moi pour en savoir plus sur mes projets passés." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <Navbar> </Navbar>
@@ -27,7 +25,7 @@ export default function Home() {
       <Experience></Experience>
       <Works></Works>
       <Contact></Contact>
-      <Footer/>
+      <Footer />
     </>
   )
 }
